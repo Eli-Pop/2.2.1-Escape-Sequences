35ee8a7e42fdb819f49ae89215052c8fcfb8de75
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-17 22:17:43 -0500

    Add files via upload

---
+++ 2.2.1 Escape Sequences.cpp
@@ -0,0 +1,15 @@
+// Demonstrate some escape sequences.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "one\ttwo\tthree\n";
+	cout << "123\b\b45";
+	//The \b\b will backspace over the 2 and 3;
+
+	cout << "\n";
+	system("pause");
+	return 0;
+}
